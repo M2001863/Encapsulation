280e1859deb36334b230dfc6a944e7b60725ef71
M2001863 <matthew.mallett4@gmail.com>
2020-12-03 15:56:48 +0000

    first commit

---
+++ src/inheritance/Inheritance.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+        Person per1 = new Person("Matthew", "Mallett", "Downtown");
+        System.out.println(per1);
+        
+    }
+    
+}
---
+++ src/inheritance/Person.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class Person {
+    
+    private String fName;
+    private String sName;
+    private String address;
+    
+    public Person(String fName, String sName, String address){
+        this.fName = fName;
+        this.sName = sName;
+        this.address = address;
+        
+        System.out.println("Sweet");
+        
+    }
+    
+   
+         
+}
